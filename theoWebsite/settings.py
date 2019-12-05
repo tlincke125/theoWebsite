ac3c580c42bb0bbdaeb2e8f8331149d804bca7e0
tlincke125 <thli2739@colorado.edu>
2019-12-04 17:18:18 -0700

    added heroku stuff

--- theoWebsite/settings.py
+++ theoWebsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,13 +21,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WANING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+#  SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY="not secret key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = True# os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = True # os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -121,3 +123,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT= os.path.join(BASE_DIR,'static/')
+
+django_heroku.settings(locals())
